1934f4ae8f8495c8ec081407f907cbfd59bdf6d3
embruna <embruna@ufl.edu>
2023-07-14 17:21:08 -0400

    still working on plot x year

--- matrix_generator.R
+++ matrix_generator.R
@@ -330,155 +330,155 @@ write_csv(plot_eta_var, "./plot_eta_var.csv")
 # eta_var_long<-pivot_longer(var_eta,sdlg:shts_7,names_to = "category") %>% rename(mean_years=value)
 
 
-# per-year per plot T ----------------------------------------
-matrix_data<-matrix_data_compiler(ha_plants)
-# How many infloresences in each year?
-infl<-matrix_data %>% group_by(year, plot_id) %>% tally(infl)%>% rename(tot_infl=n)
-
-# how many seedlings appear in each year?
-sdlg<-matrix_data %>% group_by(year, plot_id) %>% tally(sdlg==TRUE) %>% rename(tot_sdlgs=n)
-
-# The number of seedlings is based on the no. of infl. in the PREVIOUS year
-repro<-left_join(infl,sdlg) %>% 
-  mutate(tot_sdlgs=lead(tot_sdlgs)) %>% 
-  mutate(sds_per_infl=tot_sdlgs/tot_infl) %>% 
-  mutate(sds_per_infl=round(sds_per_infl,2)) %>% 
-  ungroup()
-# %>% 
-#   select(year,plot_id,sds_per_infl)
-str(repro)
-
-# There are some plots x years with no seedlings or no flowering plants, 
-# resulting in NaN or InF in seedlings per infl. Replace those values with zero
-repro<-repro %>% replace_na(list(sds_per_infl=0)) %>% 
-  mutate(sds_per_infl=case_when(
-    is.nan(sds_per_infl)~0,
-    .default = as.numeric(sds_per_infl))) %>% 
-  mutate(sds_per_infl=case_when(
-    is.infinite(sds_per_infl)~0,
-  .default = as.numeric(sds_per_infl)))
-
-# Add the reproductive data to the matrix data and muliply number of infl
-# by the number of seedlings per infl. to get value for how many 
-# seedlings in subsequent year
-matrix_data<-left_join(matrix_data,repro) %>% 
-  relocate(sds_per_infl,.after=infl) %>% 
-  mutate(seedling=infl*sds_per_infl) %>% 
-  relocate(seedling,.after=sds_per_infl)
-
-# Create a transition matrix for each year 
-plots<-sort(unique(matrix_data$plot_id))
-years<-sort(unique(matrix_data$year))
-years<-years[2]:years[(length(years)-1)]
+# # per-year per plot T ----------------------------------------
+# matrix_data<-matrix_data_compiler(ha_plants)
+# # How many infloresences in each year?
+# infl<-matrix_data %>% group_by(year, plot_id) %>% tally(infl)%>% rename(tot_infl=n)
 # 
-# data1 <- list(NA,NA,NA,NA)
-# data1 <- tibble("plot"=NA,"year"=NA,"eta"=as.list(NA),"eta_var"=as.list(NA))
-# data1 <- list("plot"=NA,"year"=NA,"eta"= NA,"eta_var"=NA)
-# data2 <- list("plot"=NA,"year"=NA,"eta"= NA,"eta_var"=NA)
+# # how many seedlings appear in each year?
+# sdlg<-matrix_data %>% group_by(year, plot_id) %>% tally(sdlg==TRUE) %>% rename(tot_sdlgs=n)
 # 
-# data3<-paste(data1, data1)
-
+# # The number of seedlings is based on the no. of infl. in the PREVIOUS year
+# repro<-left_join(infl,sdlg) %>% 
+#   mutate(tot_sdlgs=lead(tot_sdlgs)) %>% 
+#   mutate(sds_per_infl=tot_sdlgs/tot_infl) %>% 
+#   mutate(sds_per_infl=round(sds_per_infl,2)) %>% 
+#   ungroup()
+# # %>% 
+# #   select(year,plot_id,sds_per_infl)
+# str(repro)
 # 
-# data1 <- vector("list", nrow(as.data.frame(plots))*nrow(as.data.frame(years)))
-# data1 <- vector("list", nrow(as.data.frame(plots)))
-# data2 <- vector("list", nrow(as.data.frame(plots)))
-# data3 <- vector("list", nrow(as.data.frame(plots)))
-# data4 <- vector("list", nrow(as.data.frame(years)))
-
-# i=1
-# j=4
-str(matrix_data)
-for (i in seq_along(years)) {
-  for (j in seq_along(plots)) {
-  trans01<-matrix_data %>% 
-    filter(year==years[i]) %>% 
-    filter(plot_id==plots[j]) %>% 
-    filter(stage!=0) %>% 
-    filter(fate!=0) %>% 
-    select(plot_id,
-           plant_id,
-           year,
-           stage,
-           infl,
-           fate,
-           seedling) %>% 
-    mutate(stage=as.factor(stage)) %>%
-    mutate(stage=fct_relevel(stage,"sdlg","1","2","3","4","5","6","7")) %>%
-    mutate(fate=as.factor(fate)) %>% 
-    mutate(fate=fct_relevel(fate,"1","2","3","4","5","6","7","dead")) %>% 
-    mutate(seedling=replace_na(seedling,0))
-  
-  levels(trans01$stage)
-  levels(trans01$fate)
-  
-  tf <- table(trans01$fate, trans01$stage)
-  T.mat_all <- prop.table(tf, 2)
-  T.mat<-T.mat_all[1:7,]
-  F.mat <- T.mat * 0
-  F.mat[1, ] <- tapply(trans01$seedling, trans01$stage, mean)
-  
-  
-  
-  T.mat<-rbind(F.mat[1, ],T.mat)
-  rownames(T.mat)<-c("sdlg","1","2","3","4","5","6","7")
-  
-  
-  stages <- unique(matrix_data$stage)
-  fates <- unique(matrix_data$fate)
-  
-  n <- c(5,24,23,21,13,5,2,3)
-  p <- pop.projection(T.mat, n, 1000)
-  p$lambda
-  
-  eigT.mat <- eigen.analysis(T.mat)
-  eigT.mat$lambda1
-  
-  fundamental.matrix(T.mat)
-  eta<-fundamental.matrix(T.mat)
-  # eta$N
-  # eta$meaneta
-  # 
-  
-  data$eta<-eta$meaneta
-  data$eta_var<-eta$vareta
-  data<-as.data.frame(data)
-  data$stage<-row.names(data)
-  data$plot<-plots[j]
-  data$year<-years[i]
-  
-  data2<-bind_rows(data2,data)
-  }
-}
-
-
-
-plot_yr_eta <- data.frame(t(sapply(data1,c)))
-names(plot_yr_eta)<-c("sdlg","shts_1","shts_2","shts_3","shts_4","shts_5","shts_6","shts_7")
-plots <- unlist(data3)
-plots<-as_tibble(plots) %>% rename(plot=value)
-plot_yr_eta<-bind_cols(plots,plot_yr_eta)
-
-plot_yr_eta_var <- data.frame(t(sapply(data2,c)))
-names(plot_yr_eta_var)<-c("sdlg","shts_1","shts_2","shts_3","shts_4","shts_5","shts_6","shts_7")
-plot_yr_eta_var<-bind_cols(plots,plot_yr_eta_var)
-
-write_csv(plot_yr_eta, "./plot_yr_eta.csv")
-write_csv(plot_yr_eta_var, "./plot_yr_eta_var.csv")
- 
-
-
-# How many infloresences in each plot in each year?
-infl<-matrix_data %>% group_by(year, plot_id) %>% tally(infl)%>% rename(tot_infl=n)
-
-# how many seedlings appear in each plot in each year?
-sdlg<-matrix_data %>% group_by(year, plot_id) %>% tally(sdlg==TRUE) %>% rename(tot_sdlgs=n)
-
-# The number of seedlings is based on the no. of infl. in the PREVIOUS year
-repro<-left_join(infl,sdlg) %>% 
-  mutate(tot_sdlgs=lead(tot_sdlgs)) %>% 
-  mutate(sds_per_infl=tot_sdlgs/tot_infl) %>% 
-  mutate(sds_per_infl=round(sds_per_infl,2)) %>% 
-  ungroup()
-# %>% 
-#   select(year,plot_id,sds_per_infl)
-str(repro)
+# # There are some plots x years with no seedlings or no flowering plants, 
+# # resulting in NaN or InF in seedlings per infl. Replace those values with zero
+# repro<-repro %>% replace_na(list(sds_per_infl=0)) %>% 
+#   mutate(sds_per_infl=case_when(
+#     is.nan(sds_per_infl)~0,
+#     .default = as.numeric(sds_per_infl))) %>% 
+#   mutate(sds_per_infl=case_when(
+#     is.infinite(sds_per_infl)~0,
+#   .default = as.numeric(sds_per_infl)))
+# 
+# # Add the reproductive data to the matrix data and muliply number of infl
+# # by the number of seedlings per infl. to get value for how many 
+# # seedlings in subsequent year
+# matrix_data<-left_join(matrix_data,repro) %>% 
+#   relocate(sds_per_infl,.after=infl) %>% 
+#   mutate(seedling=infl*sds_per_infl) %>% 
+#   relocate(seedling,.after=sds_per_infl)
+# 
+# # Create a transition matrix for each year 
+# plots<-sort(unique(matrix_data$plot_id))
+# years<-sort(unique(matrix_data$year))
+# years<-years[2]:years[(length(years)-1)]
+# # 
+# # data1 <- list(NA,NA,NA,NA)
+# # data1 <- tibble("plot"=NA,"year"=NA,"eta"=as.list(NA),"eta_var"=as.list(NA))
+# # data1 <- list("plot"=NA,"year"=NA,"eta"= NA,"eta_var"=NA)
+# # data2 <- list("plot"=NA,"year"=NA,"eta"= NA,"eta_var"=NA)
+# # 
+# # data3<-paste(data1, data1)
+# 
+# # 
+# # data1 <- vector("list", nrow(as.data.frame(plots))*nrow(as.data.frame(years)))
+# # data1 <- vector("list", nrow(as.data.frame(plots)))
+# # data2 <- vector("list", nrow(as.data.frame(plots)))
+# # data3 <- vector("list", nrow(as.data.frame(plots)))
+# # data4 <- vector("list", nrow(as.data.frame(years)))
+# 
+# # i=1
+# # j=4
+# str(matrix_data)
+# for (i in seq_along(years)) {
+#   for (j in seq_along(plots)) {
+#   trans01<-matrix_data %>% 
+#     filter(year==years[i]) %>% 
+#     filter(plot_id==plots[j]) %>% 
+#     filter(stage!=0) %>% 
+#     filter(fate!=0) %>% 
+#     select(plot_id,
+#            plant_id,
+#            year,
+#            stage,
+#            infl,
+#            fate,
+#            seedling) %>% 
+#     mutate(stage=as.factor(stage)) %>%
+#     mutate(stage=fct_relevel(stage,"sdlg","1","2","3","4","5","6","7")) %>%
+#     mutate(fate=as.factor(fate)) %>% 
+#     mutate(fate=fct_relevel(fate,"1","2","3","4","5","6","7","dead")) %>% 
+#     mutate(seedling=replace_na(seedling,0))
+#   
+#   levels(trans01$stage)
+#   levels(trans01$fate)
+#   
+#   tf <- table(trans01$fate, trans01$stage)
+#   T.mat_all <- prop.table(tf, 2)
+#   T.mat<-T.mat_all[1:7,]
+#   F.mat <- T.mat * 0
+#   F.mat[1, ] <- tapply(trans01$seedling, trans01$stage, mean)
+#   
+#   
+#   
+#   T.mat<-rbind(F.mat[1, ],T.mat)
+#   rownames(T.mat)<-c("sdlg","1","2","3","4","5","6","7")
+#   
+#   
+#   stages <- unique(matrix_data$stage)
+#   fates <- unique(matrix_data$fate)
+#   
+#   n <- c(5,24,23,21,13,5,2,3)
+#   p <- pop.projection(T.mat, n, 1000)
+#   p$lambda
+#   
+#   eigT.mat <- eigen.analysis(T.mat)
+#   eigT.mat$lambda1
+#   
+#   fundamental.matrix(T.mat)
+#   eta<-fundamental.matrix(T.mat)
+#   # eta$N
+#   # eta$meaneta
+#   # 
+#   
+#   data$eta<-eta$meaneta
+#   data$eta_var<-eta$vareta
+#   data<-as.data.frame(data)
+#   data$stage<-row.names(data)
+#   data$plot<-plots[j]
+#   data$year<-years[i]
+#   
+#   data2<-bind_rows(data2,data)
+#   }
+# }
+# 
+# 
+# 
+# plot_yr_eta <- data.frame(t(sapply(data1,c)))
+# names(plot_yr_eta)<-c("sdlg","shts_1","shts_2","shts_3","shts_4","shts_5","shts_6","shts_7")
+# plots <- unlist(data3)
+# plots<-as_tibble(plots) %>% rename(plot=value)
+# plot_yr_eta<-bind_cols(plots,plot_yr_eta)
+# 
+# plot_yr_eta_var <- data.frame(t(sapply(data2,c)))
+# names(plot_yr_eta_var)<-c("sdlg","shts_1","shts_2","shts_3","shts_4","shts_5","shts_6","shts_7")
+# plot_yr_eta_var<-bind_cols(plots,plot_yr_eta_var)
+# 
+# write_csv(plot_yr_eta, "./plot_yr_eta.csv")
+# write_csv(plot_yr_eta_var, "./plot_yr_eta_var.csv")
+#  
+# 
+# 
+# # How many infloresences in each plot in each year?
+# infl<-matrix_data %>% group_by(year, plot_id) %>% tally(infl)%>% rename(tot_infl=n)
+# 
+# # how many seedlings appear in each plot in each year?
+# sdlg<-matrix_data %>% group_by(year, plot_id) %>% tally(sdlg==TRUE) %>% rename(tot_sdlgs=n)
+# 
+# # The number of seedlings is based on the no. of infl. in the PREVIOUS year
+# repro<-left_join(infl,sdlg) %>% 
+#   mutate(tot_sdlgs=lead(tot_sdlgs)) %>% 
+#   mutate(sds_per_infl=tot_sdlgs/tot_infl) %>% 
+#   mutate(sds_per_infl=round(sds_per_infl,2)) %>% 
+#   ungroup()
+# # %>% 
+# #   select(year,plot_id,sds_per_infl)
+# str(repro)
